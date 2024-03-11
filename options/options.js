ba15ee95f951b550151f7f6388c70651d4651057
Crowdbotics <team@crowdbotics.com>
2024-03-11 15:46:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shaan-akbar-app-46928",
+  url: "https://shaan-akbar-app-46928.botics.co",
+  api: "https://shaan-akbar-app-46928.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
